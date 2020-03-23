d12ddf59a41bdf2354da0fd00df962453aa28454
Tony Sulfaro <tony116523@gmail.com>
2020-03-22 21:50:43 -0400

    firebase config

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCgdsRpHSuS3BE0xUd0exzkdV4qCco-xKg",
+  authDomain: "first-flight-f60b5.firebaseapp.com",
+  databaseURL: "https://first-flight-f60b5.firebaseio.com",
+  projectId: "first-flight-f60b5",
+  storageBucket: "first-flight-f60b5.appspot.com",
+  messagingSenderId: "23718272440",
+  appId: "1:23718272440:web:6a8f29c8704f024011834d",
+  measurementId: "G-6VBT40RPFW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const firestore = firebase.firestore();
+
+export default firebase;
