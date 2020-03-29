68bb939828bb969ce9620411c8ea7badb695d35b
Tony Sulfaro <tony116523@gmail.com>
2020-03-29 10:38:26 -0400

    create email user

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { auth } from "../firebase";
 
 class SignUp extends Component {
-  state = { displayName: '', email: '', password: '' };
+  state = { displayName: "", email: "", password: "" };
 
   handleChange = event => {
     const { name, value } = event.target;
@@ -9,10 +10,23 @@ class SignUp extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ displayName: '', email: '', password: '' });
+    const { email, password, displayName } = this.state;
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+
+      user.updateProfile({ displayName });
+    } catch (e) {
+      console.error(e);
+    }
+
+    this.setState({ displayName: "", email: "", password: "" });
   };
 
   render() {
